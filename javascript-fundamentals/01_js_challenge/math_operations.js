72478308524887dce2a1b1794ee10e997d45f4de
KyleDDJ <kdejesus16692@student.dmmmsu.edu.ph>
2025-02-27 19:40:07 +0800

    add js challenge in javascript-fundamentals folder

---
+++ javascript-fundamentals/01_js_challenge/math_operations.js
@@ -0,0 +1,38 @@
+//1//
+item1 = 2;
+item2 = 3;
+item3 = 4;
+totalCost = (item1 + item2 + item3)
+console.log("The total cost of the items is: $" + totalCost);
+
+//2//
+num1 = 34;
+num2 = 46;
+num3 = 35;
+average = (num1 + num2 + num3);
+console.log("The average is: $" + average);
+
+//3//
+let number = 10;
+
+if (number % 2 === 0) {  
+    console.log("number is an even number.");  
+} else {  
+    console.log("number is an odd number.");  
+}
+
+//4//
+let originalPrice = 27;
+let discountPercentage = 20;
+
+let discountAmount = (originalPrice * discountPercentage) / 100 
+let discountedPrice = originalPrice - discountAmount;
+
+console.log("The discounted price is: $" + discountedPrice);
+
+//5//
+let finalPrice = 100;
+let dscntPercentage;
+
+let origPrice = finalPrice / (1 - dscntPercentage / 100);
+console.log("The original price before the discount was: $" + finalPrice);
